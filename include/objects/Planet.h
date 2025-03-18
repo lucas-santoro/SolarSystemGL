121459cc53c478734a605f129e0c2973f95e46fa
Lucas Santoro <106706839+lucas-santoro@users.noreply.github.com>
2025-03-17 21:20:23 -0300

    feat: add name property and getter

--- include/objects/Planet.h
+++ include/objects/Planet.h
@@ -1,27 +1,31 @@
 #pragma once
+#include <string>
 #include <vector>
 #include <glm/glm.hpp>
 
 class Planet
 {
 public:
-    Planet(float mass, float density, glm::vec3 position, glm::vec3 velocity, int subdivisions = 3);
+    Planet(const std::string &name, float mass, float density, glm::vec3 position, glm::vec3 velocity, int subdivisions = 3);
     ~Planet();
-    void render();
 
+    void render();
     bool intersectsRay(const glm::vec3 &rayOrigin, const glm::vec3 &rayDirection) const;
 
-    glm::vec3 getPosition() const { return position; }
-    glm::vec3 getVelocity() const { return velocity; }
-    float getRadius() const       { return radius; }
-    float getMass() const         { return mass; }
-    float getDensity() const      { return density; }
+    std::string getName() const     { return name; }
+    glm::vec3 getPosition() const   { return position; }
+    glm::vec3 getVelocity() const   { return velocity; }
+    float getRadius() const         { return radius; }
+    float getMass() const           { return mass; }
+    float getDensity() const        { return density; }
 
 private:
     unsigned int VAO, VBO, EBO;
+
     std::vector<glm::vec3> vertices;
     std::vector<unsigned int> indices;
 
+    std::string name;
     glm::vec3 position;
     float radius;
     glm::vec3 velocity;
