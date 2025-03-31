6395a7c9b5bc7823a51ac8694e280a2dda10acfb
Lucas Santoro <106706839+lucas-santoro@users.noreply.github.com>
2025-03-30 22:13:45 -0300

    feat: add planet colors when instanciating it

--- main.cpp
+++ main.cpp
@@ -39,15 +39,15 @@ int main()
     float AU = 3.0f;
 
     //planets.push_back(std::make_shared<Planet>("Sun", 1.989e30f, 1408.0f, glm::vec3(0.0f), glm::vec3(0.0f)));
-    planets.push_back(std::make_shared<Planet>("Mercury", 3.3011e23f, 5427.0f, glm::vec3(0.387f * AU, 0.0f, 0.0f), glm::vec3(0.0f)));
-    planets.push_back(std::make_shared<Planet>("Venus", 4.8675e24f, 5243.0f, glm::vec3(0.723f * AU, 0.0f, 0.0f), glm::vec3(0.0f)));
-    planets.push_back(std::make_shared<Planet>("Earth", 5.972e24f, 5514.0f, glm::vec3(1.0f * AU, 0.0f, 0.0f), glm::vec3(0.0f)));
-    planets.push_back(std::make_shared<Planet>("Moon", 7.3477e22f, 3344.0f, glm::vec3(1.0f * AU + 0.00257f * AU, 0.0f, 0.0f), glm::vec3(0.0f)));
-    planets.push_back(std::make_shared<Planet>("Mars", 6.417e23f, 3933.0f, glm::vec3(1.524f * AU, 0.0f, 0.0f), glm::vec3(0.0f)));
-    planets.push_back(std::make_shared<Planet>("Jupiter", 1.898e27f, 1326.0f, glm::vec3(5.203f * AU, 0.0f, 0.0f), glm::vec3(0.0f)));
-    planets.push_back(std::make_shared<Planet>("Saturn", 5.683e26f, 687.0f, glm::vec3(9.537f * AU, 0.0f, 0.0f), glm::vec3(0.0f)));
-    planets.push_back(std::make_shared<Planet>("Uranus", 8.681e25f, 1271.0f, glm::vec3(19.191f * AU, 0.0f, 0.0f), glm::vec3(0.0f)));
-    planets.push_back(std::make_shared<Planet>("Neptune", 1.024e26f, 1638.0f, glm::vec3(30.07f * AU, 0.0f, 0.0f), glm::vec3(0.0f)));
+    planets.push_back(std::make_shared<Planet>("Mercury", 3.3011e23f, 5427.0f, glm::vec3(0.387f * AU, 0.0f, 0.0f), glm::vec3(0.0f), glm::vec3(0.5f, 0.5f, 0.5f)));
+    planets.push_back(std::make_shared<Planet>("Venus", 4.8675e24f, 5243.0f, glm::vec3(0.723f * AU, 0.0f, 0.0f), glm::vec3(0.0f), glm::vec3(0.95f, 0.85f, 0.55f)));
+    planets.push_back(std::make_shared<Planet>("Earth", 5.972e24f, 5514.0f, glm::vec3(1.0f * AU, 0.0f, 0.0f), glm::vec3(0.0f), glm::vec3(0.2f, 0.4f, 1.0f)));
+    planets.push_back(std::make_shared<Planet>("Moon", 7.3477e22f, 3344.0f, glm::vec3(1.0f * AU + 0.00257f * AU, 0.0f, 0.0f), glm::vec3(0.0f), glm::vec3(0.6f, 0.6f, 0.6f)));
+    planets.push_back(std::make_shared<Planet>("Mars", 6.417e23f, 3933.0f, glm::vec3(1.524f * AU, 0.0f, 0.0f), glm::vec3(0.0f), glm::vec3(0.8f, 0.3f, 0.1f)));
+    planets.push_back(std::make_shared<Planet>("Jupiter", 1.898e27f, 1326.0f, glm::vec3(5.203f * AU, 0.0f, 0.0f), glm::vec3(0.0f), glm::vec3(0.9f, 0.7f, 0.4f)));
+    planets.push_back(std::make_shared<Planet>("Saturn", 5.683e26f, 687.0f, glm::vec3(9.537f * AU, 0.0f, 0.0f), glm::vec3(0.0f), glm::vec3(0.95f, 0.85f, 0.5f)));
+    planets.push_back(std::make_shared<Planet>("Uranus", 8.681e25f, 1271.0f, glm::vec3(19.191f * AU, 0.0f, 0.0f), glm::vec3(0.0f), glm::vec3(0.6f, 0.85f, 0.9f)));
+    planets.push_back(std::make_shared<Planet>("Neptune", 1.024e26f, 1638.0f, glm::vec3(30.07f * AU, 0.0f, 0.0f), glm::vec3(0.0f), glm::vec3(0.3f, 0.4f, 0.85f)));
 
 
     Grid grid(1000.0f, 300, 0.0f);
@@ -177,9 +177,12 @@ int main()
 
         if (ImGui::Button("Add Planet"))
         {
-            planets.push_back(std::make_shared<Planet>("New Planet", 1.0e24f, 3000.0f,
+            planets.push_back(std::make_shared<Planet>(
+                "New Planet", 1.0e24f, 3000.0f,
                 glm::vec3(planets.size() * 2.0f, 0.0f, 0.0f),
-                glm::vec3(0.0f)));
+                glm::vec3(0.0f),
+                glm::vec3(1.0f, 1.0f, 1.0f)
+            ));
             grid.applyGravityDistortion(planets);
         }
 
