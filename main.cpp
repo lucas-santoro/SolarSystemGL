156ff8ac2b9775fbc4018656ba02bdfe886be979
Lucas Santoro <contact@lucassantoro.com>
2025-04-16 00:45:57 +0000

    backup idk

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "core/Window.h"
+
+int main()
+{
+    Window window(800, 600, "Solar System GL");
+    window.run();
+    return 0;
+}
---
+++ src/core/Window.cpp
@@ -0,0 +1,61 @@
+#include "core/Window.h"
+
+Window::Window(int width, int height, const std::string &title)
+    : width(width), height(height), title(title), window(nullptr)
+{
+    if (!init())
+    {
+        std::cerr << "Err - Window" << std::endl;
+    }
+}
+
+Window::~Window()
+{
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+bool Window::init()
+{
+    if (!glfwInit())
+    {
+        std::cerr << "Err - GLFW" << std::endl;
+        return false;
+    }
+
+    window = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
+    if (!window)
+    {
+        std::cerr << "Err - GLFW - Window" << std::endl;
+        glfwTerminate();
+        return false;
+    }
+
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Err - GLAD" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+void Window::processInput()
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void Window::run()
+{
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput();
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+}
