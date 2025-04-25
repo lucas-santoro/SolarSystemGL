bf521c1c0025938929f0158f66cd25ad21a03256
lucas-santoro <106706839+lucas-santoro@users.noreply.github.com>
2025-04-25 16:38:01 -0300

    refactor: move UI logic to UIManager
    isolated all ImGui interface logic into a dedicated UIManager class.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include "objects/Planet.h"
 #include "core/Camera.h"
 #include "core/Grid.h"
+#include "src/ui/UIManager.h"
+
 #include <memory>
 
 void processInput(Window& window, Camera& camera, float deltaTime);
@@ -15,14 +17,13 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-bool showPlanetWindow = false;
-bool isMouseMoving = false;
-bool isMouseRotating = false;
 float lastX = 400, lastY = 300;
 bool firstMouse = true;
+bool isMouseMoving = false;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
-int selectedPlanetIndex = -1;
+
+UIManager uiManager;
 
 int main()
 {
@@ -70,8 +71,10 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         shader.use();
 
+        int width, height;
+        glfwGetFramebufferSize(window.getGLFWwindow(), &width, &height);
         glm::mat4 view = camera.getViewMatrix();
-        glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)width / std::max(height, 1), 0.1f, 100.0f);
         glm::mat4 model = glm::mat4(1.0f);
 
         shader.setMat4("view", view);
@@ -82,111 +85,17 @@ int main()
             planet->render(shader);
         }
 
-		gridShader.use();
+        gridShader.use();
         gridShader.setMat4("view", view);
         gridShader.setMat4("projection", projection);
         gridShader.setMat4("model", glm::mat4(1.0f));
         grid.draw();
 
-        double mouseX, mouseY;
-        glfwGetCursorPos(window.getGLFWwindow(), &mouseX, &mouseY);
-
-        glm::vec3 rayDirection = camera.getRayFromMouse(mouseX, mouseY, 800, 600, view, projection);
-        glm::vec3 rayOrigin = camera.getPosition();
-
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        bool isMouseOverUI = ImGui::GetIO().WantCaptureMouse;
-
-        if (!isMouseOverUI)
-        {
-            if (glfwGetMouseButton(window.getGLFWwindow(), GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS)
-            {
-                isMouseMoving = true;
-            }
-            else if (glfwGetMouseButton(window.getGLFWwindow(), GLFW_MOUSE_BUTTON_RIGHT) == GLFW_RELEASE)
-            {
-                isMouseMoving = false;
-            }
-        }
-
-        for (size_t i = 0; i < planets.size(); i++)
-        {
-            if (planets[i]->intersectsRay(camera.getPosition(), rayDirection))
-            {
-                glm::vec3 planetScreenPos = planets[i]->getPosition() + glm::vec3(0.0f, planets[i]->getRadius() + 0.2f, 0.0f);
-                glm::vec2 screenPos = camera.worldToScreen(planetScreenPos, view, projection, 800, 600);
-
-                if (screenPos.x >= 0 && screenPos.y >= 0 && screenPos.x <= 800 && screenPos.y <= 600)
-                {
-                    ImGui::SetNextWindowPos(ImVec2(screenPos.x, screenPos.y));
-                    ImGui::Begin("##PlanetName", nullptr, ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize);
-                    ImGui::Text("%s", planets[i]->getName().c_str());
-                    ImGui::End();
-
-                    if (glfwGetMouseButton(window.getGLFWwindow(), GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
-                    {
-                        showPlanetWindow = true;
-                        selectedPlanetIndex = i;
-                    }
-                }
-            }
-        }
-
-        if (showPlanetWindow && selectedPlanetIndex >= 0 && selectedPlanetIndex < planets.size())
-        {
-            auto& planet = planets[selectedPlanetIndex];
-
-            ImGui::Begin("Planet Info", &showPlanetWindow);
-
-            static char nameBuffer[128];
-            strncpy_s(nameBuffer, sizeof(nameBuffer), planet->getName().c_str(), _TRUNCATE);
-            nameBuffer[sizeof(nameBuffer) - 1] = '\0';
-
-            ImGui::InputText("Name", nameBuffer, sizeof(nameBuffer));
-            static float mass = planet->getMass();
-            static float density = planet->getDensity();
-            static float radius = planet->getRadius() * 1e7f;
-            static glm::vec3 position = planet->getPosition();
-            static glm::vec3 velocity = planet->getVelocity();
-
-            ImGui::InputFloat("Mass (kg)", &mass, 0.0f, 0.0f, "%.3e");
-            ImGui::InputFloat("Density (kg/m³)", &density);
-            ImGui::InputFloat("Radius (m)", &radius);
-            ImGui::InputFloat3("Position", &position[0]);
-            ImGui::InputFloat3("Velocity", &velocity[0]);
-
-            if (ImGui::Button("Reset"))
-            {
-                std::cout << "reset button clicked";
-            }
-            ImGui::SameLine();
-
-            if (ImGui::Button("Apply"))
-            {
-                std::cout << "apply button clicked";
-            }
-
-            ImGui::End();
-        }
-
-        ImGui::Begin("Solar System");
-        ImGui::Text("FPS: %.1f", 1.0f / deltaTime);
-
-        if (ImGui::Button("Add Planet"))
-        {
-            planets.push_back(std::make_shared<Planet>(
-                "New Planet", 1.0e24f, 3000.0f,
-                glm::vec3(planets.size() * 2.0f, 0.0f, 0.0f),
-                glm::vec3(0.0f),
-                glm::vec3(1.0f, 1.0f, 1.0f)
-            ));
-            grid.applyGravityDistortion(planets);
-        }
-
-        ImGui::End();
+        uiManager.render(window, camera, deltaTime, planets, grid);
 
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
@@ -220,8 +129,11 @@ void processInput(Window& window, Camera& camera, float deltaTime)
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
-    if (firstMouse)
-    {
+    static float lastX = xpos;
+    static float lastY = ypos;
+    static bool firstMouse = true;
+
+    if (firstMouse) {
         lastX = xpos;
         lastY = ypos;
         firstMouse = false;
@@ -233,15 +145,11 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
     lastY = ypos;
 
     if (isMouseMoving)
-    {
         camera.processMouseMovement(xoffset, yoffset);
-    }
 }
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     if (!ImGui::GetIO().WantCaptureMouse)
-    {
         camera.processKeyboard(yoffset > 0 ? GLFW_KEY_W : GLFW_KEY_S, 0.1f);
-    }
 }
