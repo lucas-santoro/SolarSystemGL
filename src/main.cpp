61fed51c2ae34f90a312fd85e9e1d14dc5e497f1
Lucas Santoro <lucas.santoro@deepseedsolutions.com>
2025-05-09 18:53:21 -0300

    feat: add a more realistic scale to planet distance

--- src/main.cpp
+++ src/main.cpp
@@ -41,13 +41,12 @@ int main()
 	Shader gridShader("shaders/GridVertexShader.glsl", "shaders/GridFragmentShader.glsl");
 
     std::vector<std::shared_ptr<Planet>> planets;
-    float AU = 3.0f;
+    float AU = 13.0f;
 
-    //planets.push_back(std::make_shared<Planet>("Sun", 1.989e30f, 1408.0f, glm::vec3(0.0f), glm::vec3(0.0f)));
     planets.push_back(std::make_shared<Planet>("Mercury", 3.3011e23f, 5427.0f, glm::vec3(0.387f * AU, 0.0f, 0.0f), glm::vec3(0.0f), glm::vec3(0.5f, 0.5f, 0.5f)));
     planets.push_back(std::make_shared<Planet>("Venus", 4.8675e24f, 5243.0f, glm::vec3(0.723f * AU, 0.0f, 0.0f), glm::vec3(0.0f), glm::vec3(0.95f, 0.85f, 0.55f)));
     planets.push_back(std::make_shared<Planet>("Earth", 5.972e24f, 5514.0f, glm::vec3(1.0f * AU, 0.0f, 0.0f), glm::vec3(0.0f), glm::vec3(0.2f, 0.4f, 1.0f)));
-    planets.push_back(std::make_shared<Planet>("Moon", 7.3477e22f, 3344.0f, glm::vec3(1.0f * AU + 0.00257f * AU, 0.0f, 0.0f), glm::vec3(0.0f), glm::vec3(0.6f, 0.6f, 0.6f)));
+    planets.push_back(std::make_shared<Planet>("Moon", 7.3477e22f, 3344.0f, glm::vec3(1.08f * AU + 0.00257f * AU, 0.0f, 0.0f), glm::vec3(0.0f), glm::vec3(0.6f, 0.6f, 0.6f)));
     planets.push_back(std::make_shared<Planet>("Mars", 6.417e23f, 3933.0f, glm::vec3(1.524f * AU, 0.0f, 0.0f), glm::vec3(0.0f), glm::vec3(0.8f, 0.3f, 0.1f)));
     planets.push_back(std::make_shared<Planet>("Jupiter", 1.898e27f, 1326.0f, glm::vec3(5.203f * AU, 0.0f, 0.0f), glm::vec3(0.0f), glm::vec3(0.9f, 0.7f, 0.4f)));
     planets.push_back(std::make_shared<Planet>("Saturn", 5.683e26f, 687.0f, glm::vec3(9.537f * AU, 0.0f, 0.0f), glm::vec3(0.0f), glm::vec3(0.95f, 0.85f, 0.5f)));
