dd1256474139446e885953ade66c6ae52d37fd06
Lucas Santoro <106706839+lucas-santoro@users.noreply.github.com>
2025-06-15 18:37:55 -0300

    feat: add orbital movement

--- src/main.cpp
+++ src/main.cpp
@@ -53,21 +53,63 @@ int main()
         glm::vec3(0.0f), glm::vec3(0.0f),
         glm::vec3(1.0f, 0.9f, 0.3f)));
 
-    planets.emplace_back(std::make_shared<Planet>("Mercury", 3.3011e23f, 5427.0f,
+    planets.emplace_back(std::make_shared<Planet>(
+        "Mercury", 3.3011e23f, 5427.0f,
         glm::vec3(0.387f * AU_WU, 0.0f, 0.0f), glm::vec3(0.0f),
         glm::vec3(0.5f, 0.5f, 0.5f)));
 
-    bodies.push_back({
-    glm::dvec3(0.0),
-    glm::dvec3(0.0),
-    1.989e30
-        });
+    planets.emplace_back(std::make_shared<Planet>(
+        "Venus", 4.8675e24f, 5243.0f,
+        glm::vec3(0.723f * AU_WU, 0.0f, 0.0f), glm::vec3(0.0f),
+        glm::vec3(0.95f, 0.85f, 0.55f)));
 
+    planets.emplace_back(std::make_shared<Planet>(
+        "Earth", 5.972e24f, 5514.0f,
+        glm::vec3(1.0f * AU_WU, 0.0f, 0.0f), glm::vec3(0.0f),
+        glm::vec3(0.2f, 0.4f, 1.0f)));
 
-    bodies.push_back({
-        glm::dvec3(0.387 * AU, 0.0, 0.0),
-        glm::dvec3(0.0, 0.0, 47'900.0),    
-        3.3011e23 });
+    planets.emplace_back(std::make_shared<Planet>(
+        "Mars", 6.417e23f, 3933.0f,
+        glm::vec3(1.524f * AU_WU, 0.0f, 0.0f), glm::vec3(0.0f),
+        glm::vec3(0.8f, 0.3f, 0.1f)));
+
+    planets.emplace_back(std::make_shared<Planet>(
+        "Jupiter", 1.898e27f, 1326.0f,
+        glm::vec3(5.203f * AU_WU, 0.0f, 0.0f), glm::vec3(0.0f),
+        glm::vec3(0.9f, 0.7f, 0.4f)));
+
+    planets.emplace_back(std::make_shared<Planet>(
+        "Saturn", 5.683e26f, 687.0f,
+        glm::vec3(9.537f * AU_WU, 0.0f, 0.0f), glm::vec3(0.0f),
+        glm::vec3(0.95f, 0.85f, 0.5f)));
+
+    planets.emplace_back(std::make_shared<Planet>(
+        "Uranus", 8.681e25f, 1271.0f,
+        glm::vec3(19.191f * AU_WU, 0.0f, 0.0f), glm::vec3(0.0f),
+        glm::vec3(0.6f, 0.85f, 0.9f)));
+
+    planets.emplace_back(std::make_shared<Planet>(
+        "Neptune", 1.024e26f, 1638.0f,
+        glm::vec3(30.07f * AU_WU, 0.0f, 0.0f), glm::vec3(0.0f),
+        glm::vec3(0.3f, 0.4f, 0.85f)));
+
+    auto addBody = [&](double a_au, double v, double m)
+        {
+            bodies.push_back({
+                glm::dvec3(a_au * AU, 0.0, 0.0),
+                glm::dvec3(0.0, 0.0, v),
+                m });
+        };
+
+    addBody(0.000, 0.0, 1.989e30); // Sol
+    addBody(0.387, 47900.0, 3.3011e23); // Mercúrio
+    addBody(0.723, 35000.0, 4.8675e24); // Vênus
+    addBody(1.000, 29780.0, 5.9720e24); // Terra
+    addBody(1.524, 24100.0, 6.4171e23); // Marte
+    addBody(5.203, 13070.0, 1.8980e27); // Júpiter
+    addBody(9.537, 9680.0, 5.6834e26); // Saturno
+    addBody(19.191, 6800.0, 8.6810e25); // Urano
+    addBody(30.070, 5430.0, 1.0240e26); // Netuno
 
     Grid grid(1000.0f, 300, 0.0f);
 
@@ -96,11 +138,33 @@ int main()
         shader.setMat4("projection", projection);
         shader.setMat4("model", model);
 
-        constexpr double TIME_SCALE = 36000.0;
+        constexpr double TIME_SCALE = 360000.0;
         double dtSim = static_cast<double>(deltaTime) * TIME_SCALE;
+        constexpr double G = 6.67430e-11;
+        constexpr double SOFTEN = 1e3;
+         
+        for (size_t i = 0; i < bodies.size(); ++i)
+        {
+            glm::dvec3 acc_m = glm::dvec3(0.0);
+
+            for (size_t j = 0; j < bodies.size(); ++j)
+            {
+                if (i == j) continue;
+
+                glm::dvec3 r = bodies[j].pos_m - bodies[i].pos_m;
+                double dist2 = glm::dot(r, r) + SOFTEN;
+                double invDist = 1.0 / sqrt(dist2);
+                acc_m += (G * bodies[j].mass_kg * invDist * invDist) * r * invDist;
+            }
+
+            bodies[i].vel_m += acc_m * dtSim;
+        }
 
         for (auto& b : bodies)
+        {
             b.pos_m += b.vel_m * dtSim;
+        }
+
 
         for (size_t i = 0; i < planets.size(); ++i)
         {
